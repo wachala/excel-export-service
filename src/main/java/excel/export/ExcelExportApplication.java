4b23b8c9e3a470420f1c8eaa4f4efe98d7b06d7d
Piotr Wachala <piotr.wachala@outlook.com>
2017-11-02 21:24:34 +0100

    Basic Eureka configuration

--- src/main/java/excel/export/ExcelExportApplication.java
+++ src/main/java/excel/export/ExcelExportApplication.java
@@ -2,8 +2,10 @@ package excel.export;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExcelExportApplication {
 
     public static void main(String[] args) {
--- src/main/java/excel/export/controller/ExportToExcelFileController.java
+++ src/main/java/excel/export/controller/ExportToExcelFileController.java
@@ -9,7 +9,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Slf4j
 @RestController
-@RequestMapping("/parking-lot")
+@RequestMapping("/api/parking-lot")
 public class ExportToExcelFileController {
 
     @RequestMapping(value = "/{id}", method = GET)
