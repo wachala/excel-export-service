1a040799847d6d646d63891cb247b12078594537
Piotr Wachala <piotr.wachala@outlook.com>
2017-11-19 15:32:18 +0100

    Swagger integration

--- src/main/java/excel/export/controller/ExportToExcelFileController.java
+++ src/main/java/excel/export/controller/ExportToExcelFileController.java
@@ -1,6 +1,7 @@
 package excel.export.controller;
 
 import excel.export.service.ExportToExcelFileService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ public class ExportToExcelFileController {
         this.excelFileService = excelFileService;
     }
 
+    @ApiOperation(value = "Returns excel file with detailed data of parking lot with given id.")
     @RequestMapping(value = "/{id}", method = GET)
     public String exportToExcelByParkingLotId(@PathVariable(name = "id") Long id) {
         log.info("Exporting to excel file for parking lot with id {}", id);
@@ -28,6 +30,7 @@ public class ExportToExcelFileController {
         return "Hello from export controller. Id passed: " + id;
     }
 
+    @ApiOperation(value = "Returns excel file with detailed data of all parking lots.")
     @RequestMapping(method = GET)
     public String exportToExcelForAllParkingLots() {
         log.info("Exporting to excel file for all parking lots");
